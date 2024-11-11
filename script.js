bf6a7a46bf61c7f053ea245410a361608cc2159e
Ganesh Pawar <ganeshppawar12@gmail.com>
2024-11-11 21:27:15 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const quote = document.getElementById('quote');
+const loading = document.getElementById('loading');
 
 async function showResponse() {
+   
+loading.style.display = 'block'
     let url = 'https://api.api-ninjas.com/v1/dadjokes';
     let headers ={ headers: {
         'X-Api-Key': 'QNnhFwBK7FgkDI13YSSDwA==SNnD4I7uu8cnKeST'
@@ -9,14 +12,19 @@ async function showResponse() {
     try{
         let res = await fetch(url,headers);
         let data = await res.json();
-    console.log(data);
+    
     quote.innerText = data[0].joke;
+    loading.innerHTML = `
+    <audio src='img/insane-funny-scream-199845.mp3' controls autoplay></audio>
+`
 
     }catch(err){
         console.log(err);
     }
-   
+    ;
+
+    loading.style.display = 'none'
+
+
 }
-
-
-showResponse();
+// showResponse();
